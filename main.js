f0a68b580fe2617f15a6d0f65d73f937fbb900f0
St4ff1-Pablo <vovadvadvavosem@gmail.com>
2022-10-17 22:41:19 +0300

    main1.0

--- main.js
+++ main.js
@@ -27,16 +27,50 @@ window.addEventListener('scroll', () => {
 
 //Поиск
 document.getElementsByClassName('search-li')[0].addEventListener('click', () => {
-    document.getElementsByClassName('search-section')[0].style.display = 'flex';
+    // document.getElementsByClassName('search-section')[0].style.display = 'flex';
+    document.getElementsByClassName('search-section')[0].style.top = '0';
     document.querySelector('body').style.overflowY = 'hidden'
 })
 document.getElementsByClassName('exit-search')[0].addEventListener('click', () => {
-    document.getElementsByClassName('search-section')[0].style.display = 'none';
+    // document.getElementsByClassName('search-section')[0].style.display = 'none';
+    document.getElementsByClassName('search-section')[0].style.top = '-120%';
     document.querySelector('body').style.overflowY = 'scroll'
 })
 //video in window
 
+
+
+
+
 //currency 
 let c = {'EUR':1,'UAH':40,'KWD':0.31,'QAR':3.64,'AED':3.67};
 let currentCurrency = document.getElementById('currency');
-//button messages
+
+
+
+
+
+
+//cart
+var cart = document.getElementById('cart-holder');
+cart.addEventListener('click', () => {
+ cart.classList.toggle('active-cart');
+
+  document.getElementsByClassName('selected-products-holder')[0].style.left='0'
+  document.getElementsByClassName('exit-selected-products')[0].style.zIndex='999'
+  document.getElementsByClassName('exit-selected-products')[0].style.backgroundColor='rgba(0, 0, 0, 0.6)'
+  document.querySelector('body').style.overflowY = 'hidden'
+
+})
+document.getElementsByClassName('exit-selected-products')[0].onclick = ()=>{
+  document.getElementsByClassName('selected-products-holder')[0].style.left='-100%'
+  document.getElementsByClassName('exit-selected-products')[0].style.zIndex='-1'
+  document.getElementsByClassName('exit-selected-products')[0].style.backgroundColor='rgba(0, 0, 0, 0)'
+  document.querySelector('body').style.overflowY = 'scroll'
+}
+document.getElementsByClassName('close-btn-selected-products')[0].onclick = ()=>{
+    document.getElementsByClassName('selected-products-holder')[0].style.left='-100%'
+    document.getElementsByClassName('exit-selected-products')[0].style.zIndex='-1'
+    document.getElementsByClassName('exit-selected-products')[0].style.backgroundColor='rgba(0, 0, 0, 0)'
+    document.querySelector('body').style.overflowY = 'scroll'
+  }
